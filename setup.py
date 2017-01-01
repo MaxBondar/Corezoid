c91d1a503bbeb77f575378e8edc1f68cc2783fa3
Max Bondar <max.bondr@gmail.com>
2017-01-02 00:46:06 +0200

    Add direct import and version

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'Corezoid',
+  packages = ['Corezoid'],
+  version = '0.1.2',
+  description = 'A library for sending and modifying task to Corezoid.',
+  author = 'Max Bondar',
+  author_email = 'max.bondr@gmail.com',
+  url = 'https://github.com/MaxBondar/Corezoid',
+  license='MIT',
+  download_url = 'https://github.com/MaxBondar/Corezoid/tarball/0.1.1',
+  keywords = ['corezoid', 'send', 'modify', 'task', 'process engine', 'event processing'],
+  classifiers = []
+)
